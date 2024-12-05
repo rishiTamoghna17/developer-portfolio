fdaa59653038c7ae67ffefe3a417a96f88374880
rishiTamoghna17 <tamoghna171099@gmail.com>
2024-12-06 00:09:20 +0530

    modified

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor of Technology",
+    duration: "2018 - 2022",
+    institution: "Siliguri institute of technology",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2016 - 2018",
+    institution: "Gobardanga khantura high school",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2006 - 2016",
+    institution: "Gobardanga Khantura high school",
   }
 ]
